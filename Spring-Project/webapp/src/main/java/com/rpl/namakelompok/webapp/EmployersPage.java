5ad690af75c27956046e82e64f2fbaca55695cfc
jaroji86 <oji.lecture@gmail.com>
2023-11-17 11:36:19 +0700

    update employers controller

--- Spring-Project/webapp/src/main/java/com/rpl/namakelompok/webapp/EmployersPage.java
+++ Spring-Project/webapp/src/main/java/com/rpl/namakelompok/webapp/EmployersPage.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EmployersPage {
     @GetMapping("/employers")
-    public String login(){
+    public String index(){
         return "employers";
     }
 }
